df4f27c9db91242fdc255fcbf597f9658cf54e51
Amshu <amshumaanrakshith@gmail.com>
2019-08-12 10:08:05 +1000

    Implemetned walking animations for the school girl.

--- LXRP_Builds/Assets/2. FirstPrototype/Scripts/CustomCharacterController.cs
+++ LXRP_Builds/Assets/2. FirstPrototype/Scripts/CustomCharacterController.cs
@@ -3,12 +3,25 @@ using UnityEngine.AI;
 
 public class CustomCharacterController : MonoBehaviour
 {
-    public Camera cam;
+    [SerializeField] Camera cam;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
 
-    public NavMeshAgent agent;
+    const float locomotionAnimationSmoothTime = .1f;
+
+    private void Start()
+    {
+        if (cam == null && agent == null && animator)
+        {
+            Debug.Log("Referrences missing in CustomCharacterController Script");
+        }
+    }
 
     void Update()
     {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
         if (Input.GetMouseButton(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -18,8 +31,6 @@ public class CustomCharacterController : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
-            
-
         }
     }
 }
