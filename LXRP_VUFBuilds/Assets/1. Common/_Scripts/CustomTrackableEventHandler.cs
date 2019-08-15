fb0a40979643ee6fd0dd9e18e02edb1ead7ab993
AmshuRak <amshurak@gmail.com>
2019-08-15 15:03:46 +1000

    Added Aboriginal painting scene and Train Model scene to LXRP_VUFBuilds
    - Added AR core functionality.

---
+++ LXRP_VUFBuilds/Assets/1. Common/_Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,53 @@
+﻿/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the DefaultTrackableEventHandler interface.
+/// </summary>
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    [SerializeField]GameObject TrackedObject = null;
+
+    protected override void Start()
+    {
+        if (TrackedObject == null)
+        {
+            Debug.Log("City object not assigned");
+            return;
+        }
+
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected override void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+
+        TrackedObject.SetActive(true);
+        //CityRise();
+    }
+}
---
+++ LXRP_VUFBuilds/Assets/2. Aboriginal Painting/OnLinkClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnLinkClick : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                print(hit.collider.name);
+                if (hit.collider.gameObject.tag == "Finish")
+                {
+                    Application.OpenURL("https://parkweb.vic.gov.au/explore/parks/you-yangs-r.p.");
+                }
+            }
+            
+
+        }
+    }
+}
