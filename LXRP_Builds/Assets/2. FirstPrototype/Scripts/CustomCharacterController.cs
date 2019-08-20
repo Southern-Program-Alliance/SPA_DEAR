9668e45d2693acf0037853497a73f7b5ab5c6ff7
AmshuRak <amshurak@gmail.com>
2019-08-20 13:31:30 +1000

    Further level load animation refinements and editor script run for character controller

--- LXRP_Builds/Assets/2. FirstPrototype/Scripts/CustomCharacterController.cs
+++ LXRP_Builds/Assets/2. FirstPrototype/Scripts/CustomCharacterController.cs
@@ -22,15 +22,28 @@ public class CustomCharacterController : MonoBehaviour
         float speedPercent = agent.velocity.magnitude / agent.speed;
         animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        #if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
         {
-            Ray ray = cam.ScreenPointToRay(Input.GetTouch(0).position);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
             }
         }
+        #else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
+        #endif
     }
 }
