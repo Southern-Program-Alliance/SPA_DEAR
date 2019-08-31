a9c1b543ff78fec275f92941b6c74c9b11dc9e38
AmshuRak <amshurak@gmail.com>
2019-08-31 14:02:01 +1000

    Car spawnner implemention complete

--- LXRP_Builds/Assets/3. SecondPrototype/CarSpawnner.cs
+++ LXRP_Builds/Assets/3. SecondPrototype/CarSpawnner.cs
@@ -4,43 +4,68 @@ using UnityEngine;
 using PathCreation;
 public class CarSpawnner : MonoBehaviour
 {
-    [SerializeField] PathCreator _path;
-    [SerializeField] List<GameObject> _listVehicle;
+    [SerializeField] PathCreator[] _arrayPath = null;
+
+    [SerializeField] int _objPoolAmt = 25;
+    [SerializeField] GameObject[] _arrayVehiclePool = null;
 
     bool _canSpawn = true;
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Get all prefabs from the list 
+        GameObject[] _vehicleArray = Resources.LoadAll<GameObject>("Vehicles");
+        // Initialize Random Seed
+        Random.InitState(System.DateTime.Now.Millisecond);
+
+        InitialzePool(_vehicleArray);
         StartCoroutine(SpawnCountdown());
     }
 
+
+    void InitialzePool(GameObject[] vehicleArray)
+    {
+        // Initialize Object Pool
+        _arrayVehiclePool = new GameObject[_objPoolAmt];
+        for (int i = 0; i < _objPoolAmt; i++)
+        {
+            GameObject car = Instantiate(vehicleArray[Random.Range(0, vehicleArray.Length)]);
+            car.SetActive(false);
+            car.transform.parent = this.gameObject.transform;
+            _arrayVehiclePool[i] = car;
+        }
+
+        //Clear array
+        System.Array.Clear(vehicleArray, 0, vehicleArray.Length);
+        vehicleArray = null;
+    }
+
+
     IEnumerator SpawnCountdown()
     {
-        while (_canSpawn)
+        for(int i = 0; i < _objPoolAmt && _canSpawn; i++)
         {
+            GameObject spawnCar = _arrayVehiclePool[i];
+
+            // Skip if already active
+            if (spawnCar.activeSelf)
+                continue;
+
+            // Add Car Script containing the Path follower code
+            CarScript spawnCarComponent = spawnCar.AddComponent<CarScript>();
+            // Choose a random path in the scene
+            PathCreator path = _arrayPath[Random.Range(0, _arrayPath.Length)];
+            spawnCarComponent.pathCreator = path;
+            spawnCarComponent.endOfPathInstruction = EndOfPathInstruction.Loop;
+            // Make it child of the path gameobject
+            spawnCar.transform.parent = path.gameObject.transform;
+
+            // Disables the shadows 
+            spawnCar.GetComponent<MeshRenderer>().receiveShadows = false;
+            spawnCar.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
             yield return new WaitForSeconds(5.0f);
-            SpawnACar();
+            spawnCar.SetActive(true);
         }
     }
-
-    void SpawnACar()
-    {
-        Debug.Log("SpawnACar");
-        if (!_canSpawn)
-            return;
-
-        Debug.Log("Spawnning a car");
-
-        // Random.seed = (int)Time.deltaTime;
-        GameObject spawnCar = Instantiate(_listVehicle[Random.Range(0, _listVehicle.Count-1)]);
-
-        PathFollower spawnCarComponent = spawnCar.AddComponent<PathFollower>();
-        spawnCarComponent.pathCreator = _path;
-        spawnCarComponent.endOfPathInstruction = EndOfPathInstruction.Loop;
-
-        spawnCar.GetComponent<MeshRenderer>().receiveShadows = false;
-        spawnCar.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-    }
-
 }
